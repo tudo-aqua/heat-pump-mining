3944927932e1545455d35e94b3aedc6cab98a4f4
Simon Dierl <simon.dierl@tu-dortmund.de>
2025-07-17 14:36:02 +0200

    disable tracking on Singularity

--- buildSrc/src/main/kotlin/tools/aqua/SingularityBuildTask.kt
+++ buildSrc/src/main/kotlin/tools/aqua/SingularityBuildTask.kt
@@ -15,9 +15,11 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.UntrackedTask
 import org.gradle.api.tasks.options.Option
 import org.gradle.process.ExecOperations
 
+@UntrackedTask(because = "Singularity inputs can not be captured")
 abstract class SingularityBuildTask @Inject constructor(private val exec: ExecOperations) :
     DefaultTask() {
   @get:InputFile abstract val specification: RegularFileProperty
