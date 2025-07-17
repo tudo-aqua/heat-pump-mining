dd56c3a67e31f6ff46d0fe4b1184f2d6495296a7
Simon Dierl <simon.dierl@tu-dortmund.de>
2025-07-17 14:35:45 +0200

    better error reporting

--- src/main/kotlin/tools/aqua/hpm/util/statistics.kt
+++ src/main/kotlin/tools/aqua/hpm/util/statistics.kt
@@ -4,11 +4,13 @@
 
 package tools.aqua.hpm.util
 
+import kotlin.Double.Companion.POSITIVE_INFINITY
 import kotlin.math.abs
 import kotlin.math.exp
 import kotlin.math.pow
 import kotlin.math.sqrt
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.ZERO
 
 fun hoeffdingTest(
     frequency1: Int,
@@ -17,7 +19,7 @@ fun hoeffdingTest(
     totalFrequency2: Int,
     epsilon: Double
 ): Boolean {
-  require(epsilon in 0.0..1.0) { "epsilon must be in [0, 1]" }
+  require(epsilon in 0.0..1.0) { "epsilon must be in [0, 1], is $epsilon" }
   return if (totalFrequency1 == 0 || totalFrequency2 == 0) {
     true
   } else {
@@ -43,10 +45,12 @@ fun hoeffdingSignificance(
     frequency2: Int,
     totalFrequency2: Int
 ): Double {
-  require(0 < totalFrequency1 && frequency1 <= totalFrequency1) { "0 < n1 && f1 <= n1 must hold" }
-  require(0 < totalFrequency2 && frequency2 <= totalFrequency2) { "0 < n2 && n2 <= n2 must hold" }
+  require(0 < totalFrequency1) { "n1 must be strictly positive, is $totalFrequency1" }
+    require(frequency1 <= totalFrequency1) { "f1 must be leq n1, is $frequency1 > $totalFrequency1" }
+    require(0 < totalFrequency2) { "n2 must be strictly positive, is $totalFrequency2" }
+    require(frequency2 <= totalFrequency2) { "f2 must be leq n2, is $frequency2 > $totalFrequency2" }
 
-  val f1 = frequency1.toDouble()
+        val f1 = frequency1.toDouble()
   val n1 = totalFrequency1.toDouble()
   val f2 = frequency2.toDouble()
   val n2 = totalFrequency2.toDouble()
@@ -63,7 +67,7 @@ fun fTest(
     blueSupport: Int,
     epsilon: Double
 ): Boolean {
-  require(epsilon >= 0) { "epsilon must be positive" }
+  require(epsilon >= 0) { "epsilon must be positive, is $epsilon" }
   if (redSupport <= 2 || blueSupport <= 1) return true
   return fTestSignificance(redAverage, redSupport, blueAverage, blueSupport) >= epsilon
 }
@@ -73,7 +77,12 @@ fun fTestSignificance(
     redSupport: Int,
     blueAverage: Duration,
     blueSupport: Int
-): Double = fTestSignificance(redAverage / blueAverage, redSupport, blueSupport)
+): Double =
+    if (redAverage == ZERO && blueAverage == ZERO) {
+      POSITIVE_INFINITY
+    } else {
+      fTestSignificance(redAverage / blueAverage, redSupport, blueSupport)
+    }
 
 fun fTestSimilarity(
     redAverage: Duration,
@@ -83,13 +92,14 @@ fun fTestSimilarity(
 ): Double =
     when {
       redSupport <= 1 || blueSupport <= 2 -> 1.0
+      redAverage == ZERO && blueAverage == ZERO -> 1.0
       else -> fTestSignificance(redAverage / blueAverage, redSupport, blueSupport).coerceAtMost(1.0)
     }
 
 fun fTestSignificance(redBlueAverageRatio: Double, redSupport: Int, blueSupport: Int): Double {
-  require(redBlueAverageRatio >= 0.0) { "tr/tb must be positive" }
-  require(redSupport > 2) { "nr must be > 2" }
-  require(blueSupport > 1) { "nb must be > 1" }
+  require(redBlueAverageRatio >= 0.0) { "tr/tb must be positive, is $redBlueAverageRatio" }
+  require(redSupport > 2) { "nr must be > 2, is $redSupport" }
+  require(blueSupport > 1) { "nb must be > 1, is $blueSupport" }
 
   val nr = redSupport.toDouble()
   val nb = blueSupport.toDouble()
